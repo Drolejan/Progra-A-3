76dd4b6a7a71275c123717265690a9fb86a6cdec
Alejandro Medina <drolejan@gmail.com>
2022-09-28 11:52:40 -0500

    Cambios finales Clase 2809

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject puntos;
-    // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(rutinaPuntos());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void spawnPoints()
     {
-        
+        Vector2 posRandom = new Vector2(Random.Range(-2, 2), Random.Range(-2, 2));
+        Instantiate(puntos,posRandom,Quaternion.identity);
+    }
+
+    IEnumerator rutinaPuntos()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            spawnPoints();
+        }
     }
 }
